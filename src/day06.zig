6357a07403efa9ca137049a12365787159665c5e
Charly Mourglia <charly.mourglia@gmail.com>
2021-12-06 20:38:45 +0100

    Solution for day06

--- src/day06.zig
+++ src/day06.zig
@@ -9,10 +9,44 @@ const Str = []const u8;
 const util = @import("util.zig");
 const gpa = util.gpa;
 
+// const data = @embedFile("../data/day06-tst.txt");
 const data = @embedFile("../data/day06.txt");
 
 pub fn main() !void {
-    
+    var it = tokenize(u8, data, ",\n\r");
+    var days = [_]u128{0} ** 9;
+
+    while (it.next()) |num| {
+        var n = try parseInt(u8, num, 10);
+        days[n] += 1;
+    }
+
+    var t0 = std.time.nanoTimestamp();
+    for ([_]u0{0} ** 512) |_| {
+        step(&days);
+    }
+    var t1 = std.time.nanoTimestamp();
+    print("{}\n", .{t1 - t0});
+
+    var total: u128 = 0;
+    for (days) |n| {
+        total += n;
+    }
+
+    print("{}", .{total});
+}
+
+fn step(days: *[9]u128) void {
+    var day0 = days[0];
+    days[0] = days[1];
+    days[1] = days[2];
+    days[2] = days[3];
+    days[3] = days[4];
+    days[4] = days[5];
+    days[5] = days[6];
+    days[6] = days[7] + day0;
+    days[7] = days[8];
+    days[8] = day0;
 }
 
 // Useful stdlib functions
