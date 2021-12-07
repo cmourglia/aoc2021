260accc934161e8344424dde7326ba0bc535b26b
Charly Mourglia <charly.mourglia@gmail.com>
2021-12-07 23:36:46 +0100

    Solution for day07

--- src/day07.zig
+++ src/day07.zig
@@ -10,9 +10,47 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("../data/day07.txt");
+// const data = @embedFile("../data/day07-tst.txt");
 
 pub fn main() !void {
-    
+    var it = tokenize(u8, data, ",\n\r");
+    var nums = List(i32).init(gpa);
+
+    while (it.next()) |num| {
+        try nums.append(try parseInt(i32, num, 10));
+    }
+
+    var min_value: i32 = std.math.maxInt(i32);
+    var max_value: i32 = 0;
+
+    for (nums.items) |n| {
+        min_value = min(min_value, n);
+        max_value = max(max_value, n);
+    }
+
+    var min_cost: i32 = std.math.maxInt(i32);
+    var min_cost_value: i32 = 0;
+
+    var i: i32 = min_value;
+    while (i <= max_value) : (i += 1) {
+        var cost: i32 = 0;
+
+        for (nums.items) |n| {
+            var steps = try std.math.absInt(i - n);
+            // part1
+            // cost += steps;
+
+            // part2
+            cost += @divExact(steps * (steps + 1), 2);
+        }
+
+        if (cost < min_cost) {
+            min_cost_value = i;
+            min_cost = cost;
+        }
+    }
+
+    print("{}\n", .{min_cost});
 }
 
 // Useful stdlib functions
